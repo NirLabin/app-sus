e9be5a5752581e0c6106e7b2a6b89fce6f3fa078
EfratZuri <efratzuri1@gmail.com>
2021-11-11 16:44:19 +0200

    add todo note added Efrat

--- js/apps/keep/cmps/note-list.js
+++ js/apps/keep/cmps/note-list.js
@@ -1,27 +1,15 @@
-import noteColors from '../cmps/note-colors.js';
-
+import notePreview from '../cmps/note-preview.js';
 export default {
 	props: ['notes'],
 	template: `
-  <section class="clean-list">
-    <ul v-if="notes&&notes.length" class="note-list">
-      <li class="note flex column space-between" v-for="note in notes" :key="note.id" :style="{
-        backgroundColor : note.style.bgc}">
-        <img v-if="note.type==='img'" :src="getSrc(note)" alt="">
-        <p v-else>{{note.txt}}</p>
-        <div class="actions">
-          <button class="btn" @click="remove(note.id)">🗑</button>
-          <button class="btn" @click="pinNote(note)">📌</button>
-          <button class="btn" @click="dupliceNote(note)">©</button>
-          <!-- <router-link class="btn" @click="send(note)" to="/mail"><ion-icon name="paper-plane"/></router-link> -->
-          <note-colors @color="changeColor" :note="note"></note-colors>
-          <input type="checkbox" v-if="note.type === 'todo' ">
-         </div>
-      </li> 
-    </ul>
-  </section>
+    <section class="clean-list">
+      <ul v-if="notes&&notes.length" class="note-list">
+        <li class="note" v-for="note in notes" :key="note.id" :style="{backgroundColor : note.style.bgc}"  >
+          <note-preview :note="note" @pin="pin" @color="changeColor" @duplicate="duplicate" @remove="remove" @todo="todo" />
+        </li> 
+      </ul>
+    </section>
   `,
-
 	data() {
 		return {
 			pinNotes: null,
@@ -31,24 +19,21 @@ export default {
 		remove(noteId) {
 			this.$emit('remove', noteId);
 		},
-		pinNote(note) {
+		pin(note) {
 			this.$emit('pin', note);
 		},
 		changeColor(color) {
 			this.$emit('color', color);
 		},
-		getSrc(note) {
-			return note.txt;
-		},
-		dupliceNote(note) {
+		duplicate(note) {
 			this.$emit('duplicate', note);
 		},
-		send(note) {
-			this.$emit('send', note);
+		todo(data) {
+			this.$emit('todo', data);
 		},
 	},
 
 	components: {
-		noteColors,
+		notePreview,
 	},
 };
--- js/apps/keep/cmps/note-todos.js
+++ js/apps/keep/cmps/note-todos.js
@@ -1,12 +1,27 @@
 export default {
-  template: `
-         <section>
-        
-        </section>
-        `,
-  data() {
-    return {};
-  },
-
-  methods: {},
+	props: ['note'],
+	template: `
+    <section class="note-todo">
+      <ul class="clean-list">
+        <li v-for="todo in note.todos" class="flex align-center gap">
+          <p @click="change(todo)" :style="todoStyle(todo)">{{todo.txt}}</p>
+        </li>
+      </ul>
+    </section>`,
+	data() {
+		return {};
+	},
+	created() {
+		console.log(this);
+		console.log(this.note);
+	},
+	methods: {
+		change(todo) {
+			// todo.isDone = !todo.isDone;
+			this.$emit('change', { todo, note: this.note });
+		},
+		todoStyle(todo) {
+			return { textDecoration: `${todo?.isDone ? 'line-through' : 'none'}` };
+		},
+	},
 };
