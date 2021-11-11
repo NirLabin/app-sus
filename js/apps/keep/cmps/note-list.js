f24266183a9e0e8a4b184622e3a160af2840eb8b
NirLabin <labinski10@gmail.com>
2021-11-11 14:10:42 +0200

    duplicate note

--- js/apps/keep/cmps/note-list.js
+++ js/apps/keep/cmps/note-list.js
@@ -1,8 +1,8 @@
 import noteColors from '../cmps/note-colors.js';
 
 export default {
-	props: ['notes'],
-	template: `
+  props: ['notes'],
+  template: `
   <section class="clean-list">
     <ul v-if="notes&&notes.length" class="note-list">
       <li class="note" v-for="note in notes" :key="note.id" :style="{
@@ -12,6 +12,7 @@ export default {
         <div class="actions">
           <button class="btn" @click="remove(note.id)">🗑</button>
           <button class="btn" @click="pinNote(note)">📌</button>
+          <button class="btn" @click="dupliceNote(note)">©</button>
           <note-colors @color="changeColor" :note="note"></note-colors>
           <input type="checkbox" v-if="note.type === 'todo' ">
          </div>
@@ -20,27 +21,30 @@ export default {
   </section>
   `,
 
-	data() {
-		return {
-			pinNotes: null,
-		};
-	},
-	methods: {
-		remove(noteId) {
-			this.$emit('remove', noteId);
-		},
-		pinNote(note) {
-			this.$emit('pin', note);
-		},
-		changeColor(color) {
-			this.$emit('color', color);
-		},
-		getSrc(note) {
-			return note.txt;
-		},
-	},
+  data() {
+    return {
+      pinNotes: null,
+    };
+  },
+  methods: {
+    remove(noteId) {
+      this.$emit('remove', noteId);
+    },
+    pinNote(note) {
+      this.$emit('pin', note);
+    },
+    changeColor(color) {
+      this.$emit('color', color);
+    },
+    getSrc(note) {
+      return note.txt;
+    },
+    dupliceNote(note) {
+      this.$emit('duplicate', note);
+    },
+  },
 
-	components: {
-		noteColors,
-	},
+  components: {
+    noteColors,
+  },
 };
--- js/apps/keep/pages/keep-page.cmp.js
+++ js/apps/keep/pages/keep-page.cmp.js
@@ -4,99 +4,109 @@ import noteAdd from '../cmps/note-add.js';
 import { noteService } from '../services/keep.service.js';
 
 export default {
-	template: `
+  template: `
         <section class="keep-app">
               <note-filter @filter="setFilter" type="search" id="filter-keeps"></note-filter>
               <note-add type="text" @add="addNewNote"></note-add>
               <note-list :notes="filterdNotes"></note-list>
-              <note-list  v-if="!filterdNotes.length" :notes="pinnedNotes" @remove="deleteNote" @pin="pinNote" @color="changeColor"></note-list>
-              <note-list v-if="!filterdNotes.length" :notes="unPinnedNotes" @remove="deleteNote" @pin="pinNote" @color="changeColor"></note-list>
+              <note-list  v-if="!filterdNotes.length" :notes="pinnedNotes" @remove="deleteNote" @pin="pinNote" @color="changeColor" @duplicate="onDuplicateNote"></note-list>
+              <note-list v-if="!filterdNotes.length" :notes="unPinnedNotes" @remove="deleteNote" @pin="pinNote" @color="changeColor" @duplicate="onDuplicateNote"></note-list>
         </section>
 
     `,
+  data() {
+    return {
+      notes: null,
+      pinnedNotes: [],
+      unPinnedNotes: [],
+      filterdNotes: [],
+      searchStr: '',
+    };
+  },
+  created() {
+    this.loadNotes();
+  },
+  methods: {
+    loadNotes() {
+      noteService.query().then((notes) => {
+        this.notes = notes;
+        this.unPinnedNotes = notes.filter((note) => !note.isPinned);
+        this.pinnedNotes = notes.filter((note) => note.isPinned);
+      });
+    },
+    addNewNote(data) {
+      const { noteTxt, noteType } = data;
+      noteService.addNote(noteTxt, noteType).then(() => this.loadNotes());
+    },
+    deleteNote(id) {
+      noteService
+        .remove(id)
+        .then((note) => {
+          noteService.updateNote(note);
+        })
+        .then(() => this.loadNotes());
+    },
 
-	data() {
-		return {
-			notes: null,
-			pinnedNotes: [],
-			unPinnedNotes: [],
-			filterdNotes: [],
-			searchStr: '',
-		};
-	},
-	created() {
-		this.loadNotes();
-	},
-	methods: {
-		loadNotes() {
-			noteService.query().then((notes) => {
-				this.notes = notes;
-				this.unPinnedNotes = notes.filter((note) => !note.isPinned);
-				this.pinnedNotes = notes.filter((note) => note.isPinned);
-			});
-		},
-		addNewNote(data) {
-			const { noteTxt, noteType } = data;
-			noteService.addNote(noteTxt, noteType).then(() => this.loadNotes());
-		},
-		deleteNote(id) {
-			noteService
-				.remove(id)
-				.then((note) => {
-					noteService.updateNote(note);
-				})
-				.then(() => this.loadNotes());
-		},
+    pinNote(note) {
+      noteService
+        .getById(note.id)
+        .then((note) => {
+          note.isPinned = !note.isPinned;
+          noteService.updateNote(note);
+        })
+        .then(() => this.loadNotes());
+    },
 
-		pinNote(note) {
-			noteService
-				.getById(note.id)
-				.then((note) => {
-					note.isPinned = !note.isPinned;
-					noteService.updateNote(note);
-				})
-				.then(() => this.loadNotes());
-		},
+    setFilter(str) {
+      this.filter = str;
+      this.filterdNotes = this.notesToShow();
+      if (!this.filter) this.filterdNotes = [];
+    },
 
-		setFilter(str) {
-			this.filter = str;
-			this.filterdNotes = this.notesToShow();
-			if (!this.filter) this.filterdNotes = [];
-		},
+    notesToShow() {
+      const searchStr = this.filter;
+      const notes = this.notes.filter((note) => {
+        let str = note.txt.toLowerCase().includes(searchStr);
+        if (!str) return;
+        this.filterdNotes.push(note);
+      });
 
-		notesToShow() {
-			const searchStr = this.filter;
-			const notes = this.notes.filter((note) => {
-				let str = note.txt.toLowerCase().includes(searchStr);
-				if (!str) return;
-				this.filterdNotes.push(note);
-			});
+      return notes;
+    },
+    changeColor(data) {
+      const { note, color } = data;
+      note.style.bgc = color;
+      noteService
+        .save(note)
+        .then((note) => this.loadNotes())
+        .catch((err) => console.log(err));
+    },
 
-			return notes;
-		},
-		changeColor(data) {
-			const { note, color } = data;
-			note.style.bgc = color;
-
-			noteService
-				.save(note)
-				.then((note) => this.loadNotes())
-				.catch((err) => console.log(err));
-		},
-	},
-	computed: {
-		// notesToShow() {
-		//   const searchStr = this.filter;
-		//   const notes = this.notes.filter((note) => {
-		//     let str = note.txt.toLowerCase().includes(searchStr);
-		//     if (note.txt.includes(str)) return note;
-		//   });
-		//   return notes;
-		// },
-	},
-	components: {
-		noteFilter,
-		noteList,
-		noteAdd,
-	},
+    onDuplicateNote(note) {
+      const newNote = { ...note };
+      newNote.id = null;
+      noteService
+        .save(newNote)
+        .then((note) => {
+          this.notes.push(note);
+          this.loadNotes();
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  computed: {
+    // notesToShow() {
+    //   const searchStr = this.filter;
+    //   const notes = this.notes.filter((note) => {
+    //     let str = note.txt.toLowerCase().includes(searchStr);
+    //     if (note.txt.includes(str)) return note;
+    //   });
+    //   return notes;
+    // },
+  },
+  components: {
+    noteFilter,
+    noteList,
+    noteAdd,
+  },
 };
