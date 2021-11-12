39949e661959ed7cc00a62fc32c2378e846bbb04
EfratZuri <efratzuri1@gmail.com>
2021-11-12 13:10:51 +0200

    change in note preview Efrat

--- js/apps/keep/cmps/note-preview.js
+++ js/apps/keep/cmps/note-preview.js
@@ -3,9 +3,9 @@ import noteTools from './note-tools.js';
 import noteEdit from '../cmps/note-edit.js';
 
 export default {
-  props: ['note'],
-  template: `
-    <div class="note-preview flex column space-between" @click="edit(note)" >
+	props: ['note'],
+	template: `
+    <div class="note-preview flex column space-between" @click="edit" >
         <note-todo v-if="note.type==='todo'" :note="note" @change="changeTodo"/>
         <img v-else-if="note.type==='img'" :src="getSrc(note)">
         <p v-else>{{note.txt}}</p>
@@ -13,45 +13,46 @@ export default {
 		<note-edit v-if="editNote" :note="note"/>
     </div>
     `,
-  data() {
-    return {
-      isHover: true,
-      editNote: false,
-    };
-  },
-  methods: {
-    getSrc(note) {
-      return note.txt;
-    },
-    toggleAction() {
-      this.isHover = !this.isHover;
-    },
-    remove() {
-      this.$emit('remove', this.note.id);
-    },
-    pin() {
-      this.$emit('pin', this.note);
-    },
-    changeColor(color) {
-      this.$emit('color', color);
-    },
-    changeTodo(data) {
-      this.$emit('todo', data);
-    },
-    duplicate() {
-      this.$emit('duplicate', this.note);
-    },
-    addTodo() {
-      this.$emit('addTodo', this.note);
-      console.log('add todo');
-    },
-    edit() {
-      this.editNote = !this.editNote;
-    },
-  },
-  components: {
-    noteTodo,
-    noteTools,
-    noteEdit,
-  },
+	data() {
+		return {
+			isHover: true,
+			editNote: false,
+		};
+	},
+	methods: {
+		getSrc(note) {
+			return note.txt;
+		},
+		toggleAction() {
+			this.isHover = !this.isHover;
+		},
+		remove() {
+			this.$emit('remove', this.note.id);
+		},
+		pin() {
+			this.$emit('pin', this.note);
+		},
+		changeColor(color) {
+			this.$emit('color', color);
+		},
+		changeTodo(data) {
+			this.$emit('todo', data);
+		},
+		duplicate() {
+			this.$emit('duplicate', this.note);
+		},
+		addTodo() {
+			this.$emit('addTodo', this.note);
+			console.log('add todo');
+		},
+		edit() {
+			if (this.editNote) return;
+			this.editNote = !this.editNote;
+		},
+	},
+	components: {
+		noteTodo,
+		noteTools,
+		noteEdit,
+	},
 };
