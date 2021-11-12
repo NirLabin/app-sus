b5bd5e85853cd4a7c7ce841ad0976d0c602f3672
NirLabin <labinski10@gmail.com>
2021-11-12 16:49:00 +0200

    style

--- js/apps/keep/cmps/note-img.js
+++ js/apps/keep/cmps/note-img.js
@@ -1,26 +1,26 @@
 export default {
-	template: `
-       <section>
-      <input type="file" accept="image/jpeg" @change=uploadImage>
-
-      </section>
+  template: `
+ <section>
+      <input type="file" accept="image/jpeg" @change=uploadImage id="upload" hidden>
+	  <label class="file-btn" for="upload"><ion-icon name="camera"></ion-icon></label>
+ </section>
       `,
-	data() {
-		return {
-			previewImage: null,
-		};
-	},
+  data() {
+    return {
+      previewImage: null,
+    };
+  },
 
-	methods: {
-		uploadImage(e) {
-			const reader = new FileReader();
-			reader.onload = (event) => {
-				let img = new Image();
-				img.src = event.target.result;
-				this.previewImage = event.target.result;
-				this.$emit('addImg', this.previewImage);
-			};
-			reader.readAsDataURL(e.target.files[0]);
-		},
-	},
+  methods: {
+    uploadImage(e) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        let img = new Image();
+        img.src = event.target.result;
+        this.previewImage = event.target.result;
+        this.$emit('addImg', this.previewImage);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    },
+  },
 };
