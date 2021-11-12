fc911c92d248c0ebaf2fda53a194e6c8a9d50d60
NirLabin <labinski10@gmail.com>
2021-11-12 18:04:41 +0200

    clean css and html

--- js/cmps/user-msg.cmp.js
+++ js/cmps/user-msg.cmp.js
@@ -1,34 +1,34 @@
 import { eventBus } from '../services/event-bus-service.js';
 
 export default {
-	template: `
+  template: `
     <transition name="fade">
         <div v-if="msg" class="user-msg">
             <p>{{msg.txt}}</p>
             <div class="close-msg" @click="closeMsg"><ion-icon name="close-circle-outline"></ion-icon></div>
         </div>
     </transition>`,
-	data() {
-		return {
-			msg: null,
-		};
-	},
-	created() {
-		eventBus.$on('showMsg', this.showMsg);
-	},
-	destroyed() {
-		eventBus.$off('showMsg', this.showMsg);
-	},
-	methods: {
-		showMsg(msg) {
-			this.msg = msg;
-			setTimeout(() => {
-				this.msg = null;
-			}, 3500);
-		},
+  data() {
+    return {
+      msg: null,
+    };
+  },
+  created() {
+    eventBus.$on('showMsg', this.showMsg);
+  },
+  destroyed() {
+    eventBus.$off('showMsg', this.showMsg);
+  },
+  methods: {
+    showMsg(msg) {
+      this.msg = msg;
+      setTimeout(() => {
+        this.msg = null;
+      }, 3500);
+    },
 
-		closeMsg() {
-			this.msg = null;
-		},
-	},
+    closeMsg() {
+      this.msg = null;
+    },
+  },
 };
