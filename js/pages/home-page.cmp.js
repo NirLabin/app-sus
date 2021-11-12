ce4d6abdfafa2d33fad6be047f2a05459c871be3
NirLabin <labinski10@gmail.com>
2021-11-12 17:44:09 +0200

    home page transtion

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,18 +1,20 @@
 export default {
-	template: `
+  template: `
     <section class="home-page">
       <header class="app-header flex space-between align-center column">
         <h1>APP SUS</h1>
-        <div class="home-body flex">
-          <router-link to="/mail" active-class="active" class="flex column align-center">
-            <img class="menu-img"src="https://play-lh.googleusercontent.com/KSuaRLiI_FlDP8cM4MzJ23ml3og5Hxb9AapaGTMZ2GgR103mvJ3AAnoOFz1yheeQBBI" alt="">
-            <span>Mail</span>
-          </router-link> 
-          <router-link to="/keep" active-class="active" class="flex column align-center">
-            <img class ="menu-img"src="https://www.google.com/images/icons/product/keep-512.png" alt="">
-            <span>Keep</span>
-          </router-link>
-        </div>
+        <transition name="home page">
+          <div class="home-body flex">
+            <router-link to="/mail" active-class="active" class="flex column align-center">
+              <img class="menu-img"src="https://play-lh.googleusercontent.com/KSuaRLiI_FlDP8cM4MzJ23ml3og5Hxb9AapaGTMZ2GgR103mvJ3AAnoOFz1yheeQBBI" alt="">
+              <span>Mail</span>
+            </router-link> 
+            <router-link to="/keep" active-class="active" class="flex column align-center">
+              <img class ="menu-img"src="https://www.google.com/images/icons/product/keep-512.png" alt="">
+              <span>Keep</span>
+            </router-link>
+          </div>
+        </transition>
         </header>
     </section>
     `,
