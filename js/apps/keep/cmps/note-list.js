6891b2eb0cdaa6e8838005ca8771282c32deb8cc
NirLabin <labinski10@gmail.com>
2021-11-13 19:57:49 +0200

    fix bugs

--- js/apps/keep/cmps/note-list.js
+++ js/apps/keep/cmps/note-list.js
@@ -2,8 +2,8 @@ import notePreview from '../cmps/note-preview.js';
 import noteEdit from '../cmps/note-edit.js';
 
 export default {
-	props: ['notes'],
-	template: `
+  props: ['notes'],
+  template: `
     <section v-if="notes" class="note-list-container" >
         <h3 v-if="notes.pinned.length && notes.unPinned.length">Pinned:</h3>
 		<ul v-if="notes && notes.pinned.length" class="note-list">
@@ -13,55 +13,55 @@ export default {
 		</ul>
 		<h3 v-if="notes.pinned.length && notes.unPinned.length">Unpinned:</h3>
 		<ul v-if="notes && notes.unPinned.length" class="note-list">
-			<li class="note vivify fadeInTop" v-for="note in notes.unPinned" :key="note.id" :style="{backgroundColor : note?.style?.bgc}" >
+			<li class="note " v-for="note in notes.unPinned" :key="note.id" :style="{backgroundColor : note?.style?.bgc}" >
 				<note-preview :note="note" @edit="edit" @pin="pin" @color="changeColor" @duplicate="duplicate" @remove="remove" @todo="todo" @addTodo="addTodo"  @save="save" @send="send"/>
 			</li> 
 		</ul>
 		<note-edit v-if="editNote" :note="editNote" @close="closeEdit" @save="save" @remove="remove" @pin="pin" @duplicate="duplicate" @addTodo="addTodo" @send="send" @color="changeColor" />
     </section>`,
 
-	data() {
-		return {
-			editNote: null,
-		};
-	},
+  data() {
+    return {
+      editNote: null,
+    };
+  },
 
-	methods: {
-		addTodo(note) {
-			this.$emit('addTodo', note);
-		},
-		remove(noteId) {
-			this.$emit('remove', noteId);
-		},
-		pin(note) {
-			this.$emit('pin', note);
-		},
-		changeColor(color) {
-			this.$emit('color', color);
-		},
-		duplicate(note) {
-			this.$emit('duplicate', note);
-		},
-		todo(data) {
-			this.$emit('todo', data);
-		},
-		save(data) {
-			this.editNote = null;
-			this.$emit('save', data);
-		},
-		send(note) {
-			this.$emit('send', note);
-		},
-		closeEdit() {
-			this.editNote = null;
-		},
-		edit(note) {
-			if (this.editNote || note.type === 'img') return;
-			this.editNote = note;
-		},
-	},
-	components: {
-		notePreview,
-		noteEdit,
-	},
+  methods: {
+    addTodo(note) {
+      this.$emit('addTodo', note);
+    },
+    remove(noteId) {
+      this.$emit('remove', noteId);
+    },
+    pin(note) {
+      this.$emit('pin', note);
+    },
+    changeColor(color) {
+      this.$emit('color', color);
+    },
+    duplicate(note) {
+      this.$emit('duplicate', note);
+    },
+    todo(data) {
+      this.$emit('todo', data);
+    },
+    save(data) {
+      this.editNote = null;
+      this.$emit('save', data);
+    },
+    send(note) {
+      this.$emit('send', note);
+    },
+    closeEdit() {
+      this.editNote = null;
+    },
+    edit(note) {
+      if (this.editNote || note.type === 'img') return;
+      this.editNote = note;
+    },
+  },
+  components: {
+    notePreview,
+    noteEdit,
+  },
 };
